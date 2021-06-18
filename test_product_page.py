6301aac06b0165dd23672247415e571bc8e6e019
dracas <dowz2012@gmail.com>
2021-06-18 10:11:01 +0300

    Добавил тест-кейс для проверки, что в корзине нету товара, если пользователь его не добавил.

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 import pytest        # необходимо будет убрать, т.к. уже есть импорт в conftest, сейчас нужна чтобы PC не ругался
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 @pytest.mark.parametrize(
@@ -25,7 +26,7 @@ def test_guest_can_add_product_to_basket(browser, link):
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_an_opportunity_add_product_to_basket()
@@ -33,7 +34,7 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 
 def test_guest_cant_see_success_message(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_message()
@@ -41,7 +42,7 @@ def test_guest_cant_see_success_message(browser):
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/hacking-exposed-wireless_208/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_an_opportunity_add_product_to_basket()
@@ -49,16 +50,25 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/catalogue/google-hacking_197/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, link)
+    basket_page.should_be_not_products_and_be_message()
